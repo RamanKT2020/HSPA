43c324b61181fb56b1df544987d8e68cb5146fb8
Raman Thatikunta <raman_k_t@hotmail.com>
2020-09-24 06:48:04 -0400

    20 completed - add login/logout functionality

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -16,6 +16,9 @@ import { UserRegisterComponent } from './user/user-register/user-register.compon
 import { UserLoginComponent } from './user/user-login/user-login.component';
 import { UserService } from './services/user.service';
 import { AlertifyService } from './services/alertify.service';
+import { AuthService } from './services/auth.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 const appRoutes: Routes  = [
@@ -48,9 +51,14 @@ const appRoutes: Routes  = [
     HttpClientModule,
     FormsModule, //for template-driven forms
     ReactiveFormsModule, // for reactive forms
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
-  providers: [HousingService, UserService, AlertifyService],
+  providers: [HousingService
+              , UserService
+              , AlertifyService
+              , AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
