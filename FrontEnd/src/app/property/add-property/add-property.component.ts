48fa59ca1072c2f9689966496ee8725c55df18e6
Raman Thatikunta <raman_k_t@hotmail.com>
2020-09-29 20:05:39 -0400

    Error Cannot bind ngModelOptions

--- FrontEnd/src/app/property/add-property/add-property.component.ts
+++ FrontEnd/src/app/property/add-property/add-property.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
 //import { IProperty } from 'src/app/model/iproperty';
@@ -14,8 +14,12 @@ import { IPropertyBase } from 'src/app/model/ipropertybase';
 })
 export class AddPropertyComponent implements OnInit {
 
-  @ViewChild('Form') addPropertyForm: NgForm;
+  //@ViewChild('Form') addPropertyForm: NgForm; //used for template-driven form;
   @ViewChild('formTabs') formTabs: TabsetComponent;
+
+  addPropertyForm: FormGroup;
+  nextClicked: boolean;
+
 
   propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex'];
   furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
@@ -34,9 +38,54 @@ export class AddPropertyComponent implements OnInit {
 
   };
 
-  constructor(private router : Router) { }
+  constructor(private fb: FormBuilder, private router : Router) { }
 
   ngOnInit() {
+    this.createAddPropertyForm();
+  }
+
+  createAddPropertyForm() {
+    this.addPropertyForm = this.fb.group({
+
+      BasicInfo: this.fb.group({
+        SellRent: [null, Validators.required],
+        PType: [null, Validators.required],
+        Name: [null, Validators.required]
+      }),
+      PriceInfo: this.fb.group({
+        Price: [null, Validators.required],
+        BuiltArea: [null, Validators.required]
+      })
+
+    });
+  }
+
+  // ------------------
+  //  Getter methods
+  //-------------------
+
+  get BasicInfo() {
+    return this.addPropertyForm.controls.BasicInfo as FormGroup;
+  }
+  get SellRent() {
+    return this.BasicInfo.controls.SellRent as FormControl;
+  }
+
+  get BHK() {
+    return this.BasicInfo.controls.BHK as FormControl;
+  }
+
+  get RTM() {
+    return this.BasicInfo.controls.RTM as FormControl;
+  }
+
+
+  get PriceInfo() {
+    return this.addPropertyForm.controls.PriceInfo as FormGroup;
+  }
+
+  get Price() {
+    return this.PriceInfo.controls.Price as FormControl;
   }
 
   onSubmit() {
@@ -44,6 +93,8 @@ export class AddPropertyComponent implements OnInit {
     console.log('SellRent=' + this.addPropertyForm.value.BasicInfo.SellRent);
     console.log(this.addPropertyForm);
   }
+
+
 
   //  This way of referencing the Form works as well
   // onSubmit(Form: NgForm) {
@@ -56,6 +107,9 @@ export class AddPropertyComponent implements OnInit {
   }
 
   selectTab(tabId: number) {
-    this.formTabs.tabs[tabId].active = true;
+    this.nextClicked = true;
+    //if (IsCurrentTabValid) {
+      this.formTabs.tabs[tabId].active = true;
+    //}
   }
 }
