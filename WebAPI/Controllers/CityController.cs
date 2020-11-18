26ce4eb99cd4dcaa7c6dd8588aa0cb2dcff83e75
Raman Thatikunta <raman_k_t@hotmail.com>
2020-11-17 20:49:41 -0500

    33-2 completed; add and delete methods in a controller

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -29,9 +29,18 @@ namespace WebAPI.Controllers
             return Ok(cities);
         }
         
+        /* 
+            3 ways to add an object:
+
+            1. Pass the info in querystring
+            2. Pass the info in URL
+            3. Pass the info in the body as JSON 
+        */
         
         // post api/city/add?cityname=Miami
+        // post api/city/add/Los Angeles
         [HttpPost("add")]
+        [HttpPost("add/{cityname}")]
         public async Task<IActionResult> AddCity(string cityName)
         {
             City city = new City();
@@ -41,6 +50,28 @@ namespace WebAPI.Controllers
             await dc.SaveChangesAsync();
 
             return Ok(city);
+        }        
+        
+        // post api/city/post
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            
+            await dc.Cities.AddAsync(city);
+            await dc.SaveChangesAsync();
+
+            return Ok(city);
         }
+
+        // delete api/city/delete/10
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await dc.Cities.FindAsync(id);
+            dc.Cities.Remove(city);
+            await dc.SaveChangesAsync();
+
+            return Ok(id);
+        }        
     }
 }
