5550252139fd865e25f077562084afe0bb02eb97
Raman Thatikunta <raman_k_t@hotmail.com>
2020-11-19 06:46:37 -0500

    34 completed; Repository pattern implemented

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using WebAPI.Data;
+using WebAPI.Data.Repo;
 using WebAPI.Models;
 //using WebAPI.Models;
 
@@ -14,17 +13,18 @@ namespace WebAPI.Controllers
     [ApiController]
     public class CityController : ControllerBase
     {
-        private readonly DataContext dc;
-        public CityController(DataContext dc)
+        private readonly ICityRepository repo;
+
+        public CityController(ICityRepository repo)
         {
-            this.dc = dc;
+            this.repo = repo;
         }
 
         // get api/city
         [HttpGet("")]
         public async Task<IActionResult> GetCities()
         {
-            var cities = await dc.Cities.ToListAsync();
+            var cities = await repo.GetCitiesAsync();
 
             return Ok(cities);
         }
@@ -36,41 +36,37 @@ namespace WebAPI.Controllers
             2. Pass the info in URL
             3. Pass the info in the body as JSON 
         */
-        
+        //commented out 1 and 2 types because we only need one way to add a city, type 3
+
         // post api/city/add?cityname=Miami
         // post api/city/add/Los Angeles
-        [HttpPost("add")]
-        [HttpPost("add/{cityname}")]
-        public async Task<IActionResult> AddCity(string cityName)
-        {
-            City city = new City();
-            city.Name = cityName;
+        // [HttpPost("add")]
+        // [HttpPost("add/{cityname}")]
+        // public async Task<IActionResult> AddCity(string cityName)
+        // {
+        //     City city = new City();
+        //     city.Name = cityName;
             
-            await dc.Cities.AddAsync(city);
-            await dc.SaveChangesAsync();
+        //     repo.AddCity(city);
 
-            return Ok(city);
-        }        
+        //     return Ok(city);
+        // }        
         
         // post api/city/post
         [HttpPost("post")]
         public async Task<IActionResult> AddCity(City city)
         {
-            
-            await dc.Cities.AddAsync(city);
-            await dc.SaveChangesAsync();
-
-            return Ok(city);
+            repo.AddCity(city);
+            await repo.SaveAsync();
+            return StatusCode(201);
         }
 
         // delete api/city/delete/10
         [HttpDelete("delete/{id}")]
         public async Task<IActionResult> DeleteCity(int id)
         {
-            var city = await dc.Cities.FindAsync(id);
-            dc.Cities.Remove(city);
-            await dc.SaveChangesAsync();
-
+            repo.DeleteCity(id);
+            await repo.SaveAsync();
             return Ok(id);
         }        
     }
---
+++ WebAPI/Data/Repo/ICityRepository.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Data.Repo
+{
+    public interface ICityRepository
+    {
+         Task<IEnumerable<City>> GetCitiesAsync();
+
+         //Not asynchronous because the method is adding City to memory only
+         void AddCity(City city);
+
+         void DeleteCity(int cityId);
+
+        //This should be in UnitOfWork
+         Task<bool> SaveAsync();
+    }
+}
