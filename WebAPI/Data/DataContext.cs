bea12ff9d860b174992403f62be2399934008917
Raman Thatikunta <raman_k_t@hotmail.com>
2020-12-16 06:13:34 -0500

    added .gtignore in webAPI project; added comment in DataContext class

--- WebAPI/Data/DataContext.cs
+++ WebAPI/Data/DataContext.cs
@@ -6,6 +6,7 @@ namespace WebAPI.Data
     public class DataContext: DbContext
     {
       //public DataContext() {} // you get a different error with this
+      //https://www.youtube.com/watch?v=j1e6Z-7QNpk&lc=Ugz3TZMlrg6t67COxnh4AaABAg.9G5_T2ViJlC9HK8dkiyOea
       public DataContext(DbContextOptions<DataContext> options) : base(options) { }  
 
       public DbSet<City> Cities { get; set; }
